ae33a09c8a8cbb5b58246ba6dd2b31141b9a680a
Theo McMullien <theomcmullien.16@gmail.com>
2022-06-01 23:47:38 +1200

    login controller

---
+++ PanGainsWebApp/Controllers/LoginController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PanGainsWebApp.Models;
+using System.Diagnostics;
+
+namespace PanGainsWebApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly ILogger<LoginController> _logger;
+
+        public LoginController(ILogger<LoginController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ PanGainsWebApp/Models/ListModel.cs
@@ -0,0 +1,19 @@
+﻿namespace PanGainsWebApp.Models
+{
+    public class ListModel
+    {
+        public IList<Account>? AccountModel { get; set; }
+        public IList<ChallengeStats>? ChallengeStatsModel { get; set; }
+        public IList<CompletedWorkout>? CompletedWorkoutModel { get; set; }
+        public IList<DaysWorkedOut>? DaysWorkedOutModel { get; set; }
+        public IList<Exercise>? ExerciseModel { get; set; }
+        public IList<Folder>? FolderModel { get; set; }
+        public IList<Leaderboard>? LeaderboardModel { get; set; }
+        public IList<Routine>? RoutineModel { get; set; }
+        public IList<Set>? SetModel { get; set; }
+        public IList<Social>? SocialModel { get; set; }
+        public IList<Statistics>? StatisticsModel { get; set; }
+        public IList<YourExercise>? YourExerciseModel { get; set; }
+
+    }
+}
