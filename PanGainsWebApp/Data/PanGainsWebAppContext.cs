33d4f55dd59711c5744b8ac5c95d6557997cf5d2
Theo McMullien <theomcmullien.16@gmail.com>
2022-06-03 15:29:08 +1200

    bob

--- PanGainsWebApp/Data/PanGainsWebAppContext.cs
+++ PanGainsWebApp/Data/PanGainsWebAppContext.cs
@@ -13,18 +13,18 @@ namespace PanGainsWebApp.Data
         {
         }
 
-        public DbSet<Account> Account { get; set; }
-        public DbSet<ChallengeStats> ChallengeStats { get; set; }
-        public DbSet<CompletedWorkout> CompletedWorkout { get; set; }
-        public DbSet<DaysWorkedOut> DaysWorkedOut { get; set; }
-        public DbSet<Exercise> Exercise { get; set; }
-        public DbSet<Folder> Folder { get; set; }
-        public DbSet<Leaderboard> Leaderboard { get; set; }
-        public DbSet<Routine> Routine { get; set; }
-        public DbSet<Set> Set { get; set; }
-        public DbSet<Social> Social { get; set; }
-        public DbSet<Statistics> Statistics { get; set; }
-        public DbSet<YourExercise> YourExercise { get; set; }
+        public DbSet<Account>? Account { get; set; }
+        public DbSet<ChallengeStats>? ChallengeStats { get; set; }
+        public DbSet<CompletedWorkout>? CompletedWorkout { get; set; }
+        public DbSet<DaysWorkedOut>? DaysWorkedOut { get; set; }
+        public DbSet<Exercise>? Exercise { get; set; }
+        public DbSet<Folder>? Folder { get; set; }
+        public DbSet<Leaderboard>? Leaderboard { get; set; }
+        public DbSet<Routine>? Routine { get; set; }
+        public DbSet<Set>? Set { get; set; }
+        public DbSet<Social>? Social { get; set; }
+        public DbSet<Statistics>? Statistics { get; set; }
+        public DbSet<YourExercise>? YourExercise { get; set; }
 
     }
 }
